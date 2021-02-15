2e9b7d25111804550a9b67a90aa0c8aec61866cd
FLC-Neiyko <74118771+FLC-Neiyko@users.noreply.github.com>
2021-02-15 21:51:40 +0100

    Initial commit

--- src/main/java/fr/neiyko/basiccmd/manager/MFiles.java
+++ src/main/java/fr/neiyko/basiccmd/manager/MFiles.java
@@ -15,37 +15,43 @@ public class MFiles {
 
     public void initFiles() {
 
-        ressourceSetup("config.yml", true);
+        ressourceSetup("config.yml", false);
         main.fileConfigConfiguration = YamlConfiguration.loadConfiguration(main.configFile);
 
-        ressourceSetup("messages.yml", true);
+        ressourceSetup("messages.yml", false);
         main.fileConfigMessages = YamlConfiguration.loadConfiguration(main.messagesFile);
 
     }
 
     public void ressourceSetup(String fileName, boolean reset) {
-        InputStream in = main.getResource(fileName);
-        if (in == null)
-            throw new IllegalArgumentException("The '" + fileName + "' ressource was not found!");
+        InputStream in = main.getResource((fileName));
+
+        if (in == null) {
+            throw new IllegalArgumentException("The '" + fileName + "' resource was not found !");
+        }
+
         File outDir = new File(main.getDataFolder(), "");
-        if (!outDir.exists())
+        if (!outDir.exists()) {
             outDir.mkdirs();
+        }
         String fileNameString = fileName.toLowerCase();
-        if (fileNameString.equals("config.yml") || fileNameString.equals("messages.yml")) {
+        if (fileNameString.equals("config.yml") || fileName.equals("messages.yml")) {
             File outFile = new File(main.getDataFolder(), fileName);
-            if (!outDir.exists())
+            if (!outFile.exists() || reset) {
                 try {
                     OutputStream out = new FileOutputStream(outFile);
-                    byte[] buf = new byte[63];
+                    byte[] buf = new byte['?'];
                     int len;
-                    while ((len = in.read(buf)) > 0)
+                    while ((len = in.read(buf)) > 0) {
                         out.write(buf, 0, len);
+                    }
                     out.close();
                     in.close();
                     return;
                 } catch (Exception e) {
-                    main.logConsole(Level.WARNING, "The '" + fileName + "' was not found!");
+                    main.logConsole(Level.WARNING, "The '" + fileName + "' was not found !");
                 }
+            }
         }
     }
 }
